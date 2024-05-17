c77b1cfb555dc9bedfdae43e844eda2e595d086d
sacalvario <simon.calvario0986@alumnos.udg.mx>
2024-05-17 13:51:59 -0600

    por que mi vida se apaga....

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Media;
 
 #nullable disable
 
@@ -16,6 +17,39 @@ namespace ProjectManager.Models
         public string Name { get; set; }
         public float? Predecessor { get; set; }
         public float? Number { get; set; }
+
+        private SolidColorBrush _NumberColor;
+        public SolidColorBrush NumberColor
+        {
+            get
+            {
+                if (Number == 1)
+                {
+                    _NumberColor = new SolidColorBrush(Colors.LimeGreen);
+                }
+                else if (Number == 1.2)
+                {
+                    _NumberColor = new SolidColorBrush(Colors.DarkOrange);
+                }
+                else if (Number == 2)
+                {
+                    _NumberColor = new SolidColorBrush(Colors.OrangeRed);
+                }
+                else if (Number == 3)
+                {
+                    _NumberColor = new SolidColorBrush(Color.FromRgb(100, 184, 0));
+                }
+                else if (Number == 4)
+                {
+                    _NumberColor = new SolidColorBrush(Colors.Red);
+                }
+                else if (Number == 5)
+                {
+                    _NumberColor = new SolidColorBrush(Colors.Red);
+                }
+                return _NumberColor;
+            }
+        }
 
         public virtual ICollection<ProjectTask> ProjectTaskIdTaskNavigations { get; set; }
     }
